be4bf91733eae55dbe0544584c6a82d867d6a82c
Commander-Cat101 <bendabomber19@gmail.com>
2025-09-29 15:58:10 +1300

    Bump version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BloonFactory;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "50.1";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "BloonFactory";
 
     public const string Description = "A mod for creating custom bloons";
