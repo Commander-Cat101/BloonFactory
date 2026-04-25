f1eb5e0325001f1534c6d64e97c3e4e2541bb3e8
Commander-Cat101 <bendabomber19@gmail.com>
2026-04-25 22:53:18 +1200

    actually fixed for v54

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BloonFactory;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "53";
-    public const string Version = "1.1.0";
+    public const string Version = "1.1.1";
     public const string Name = "BloonFactory";
 
     public const string Description = "A mod for creating custom bloons";
